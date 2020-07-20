a17f68ed4242f2b78987b2cb5844371992a5c7fc
Bernard Polynice <bepoly.nice@gmail.com>
2020-07-20 03:34:02 -0400

    Initial lab 3 commit

---
+++ BernardLab3/Calculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BernardLab3
+{
+    class Calculation
+    {
+        public static void CalculateOddEven(string userName, double userNumber)
+        {
+            if (userNumber % 2 != 0 && userNumber <= 100 && userNumber >= 1)
+            {
+                Console.WriteLine($"{userName}, {userNumber} is Odd.");
+            }
+            else if (userNumber % 2 == 0 && 2 <= userNumber && userNumber <= 25)
+            {
+                Console.WriteLine($"{userName}, {userNumber} is Even and less than 25.");
+            }
+            else if (userNumber % 2 == 0 && 26 <= userNumber && userNumber <= 60)
+            {
+                Console.WriteLine($"{userName}, it's Even.");
+            }
+            else if (userNumber % 2 == 0 && userNumber > 60 && userNumber <= 100)
+            {
+                Console.WriteLine($"{userName}, {userNumber} is Even.");
+            }
+            else if (userNumber % 2 != 0 && userNumber > 60 && userNumber <= 100)
+            {
+                Console.WriteLine($"{userName}, {userNumber} is Odd.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{userName}, {userNumber} is outside the scope of this program.");
+            }
+
+        }
+    }
+}
---
+++ BernardLab3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BernardLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HELLO THERE!! Before we get started, what is your name? ");
+            string userName = Console.ReadLine().ToUpper();
+            Console.Write($"Pleasure to meet you {userName}! ");
+            string continueYesNo = "y";
+
+            while (continueYesNo == "y" || continueYesNo == "")
+            {
+                Console.Write("Enter a number between 1 and 100: ");
+                try
+                {
+                    double userNumber = double.Parse(Console.ReadLine());
+
+                    Calculation.CalculateOddEven(userName, userNumber);
+                }
+                catch (Exception numberOutOfLimit)
+                {
+
+                    Console.WriteLine(numberOutOfLimit.Message);
+                }
+
+                Console.Write($"Would you like to continue {userName}? (y/n) ");
+                continueYesNo = Console.ReadLine().ToLower().Trim();
+            }
+            Console.WriteLine($"Goodbye {userName}!!!  Its been REAL!!!");
+
+        }
+
+    }
+}
